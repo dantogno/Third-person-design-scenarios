f263f1c9b3e0fecc583e02b3f76d2f8e19678d7b
David Antognoli <david.antognoli@gmail.com>
2020-09-15 11:24:19 -0500

    first pass on moving character with AddForce

---
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 2;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        UpdateInput();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+        rigidbody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
+    }
+    private void UpdateInput()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+    }
+}
