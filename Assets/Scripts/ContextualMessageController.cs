9526a480cc5859821ba0aa65ecc3ef97bf293281
David Antognoli <david.antognoli@gmail.com>
2020-09-30 12:00:34 -0500

    Create contextual message controller script
    - create ContextualMessageController script
    - add to game object
    - add canvas group
    - initialize components
    - stub in showMessage

---
+++ Assets/Scripts/ContextualMessageController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ContextualMessageController : MonoBehaviour
+{
+    private TMP_Text text;
+    private CanvasGroup canvasGroup;
+
+    private void Start()
+    {
+        text = GetComponent<TMP_Text>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void ShowText(string message, float duration)
+    {
+
+    }
+}
