216fafa6bd9ea7417b4ed618a872d2870515f3a7
David Antognoli <david.antognoli@gmail.com>
2020-09-30 14:35:22 -0500

    Add parameters to delegate and serialize fields for them

--- Assets/Scripts/ContextualMessageTrigger.cs
+++ Assets/Scripts/ContextualMessageTrigger.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 
 public class ContextualMessageTrigger : MonoBehaviour
 {
-    public delegate void ContextualMessageTriggeredAction();
+    [SerializeField]
+    [TextArea(3, 5)]
+    private string message = "hello";
+
+    [SerializeField]
+    private float messageDuration = 2;
+
+    public delegate void ContextualMessageTriggeredAction(string message, float duration);
 
     public static event ContextualMessageTriggeredAction ContextualMessageTriggered;
 
     private void OnTriggerEnter(Collider other)
     {
         if (ContextualMessageTriggered != null)
-            ContextualMessageTriggered.Invoke();
+            ContextualMessageTriggered.Invoke(message, messageDuration);
     }
 }
