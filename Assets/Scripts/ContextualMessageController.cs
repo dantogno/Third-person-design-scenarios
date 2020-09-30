3d1b39f3bfc10917af0ea6ef2583c81c35b10e05
David Antognoli <david.antognoli@gmail.com>
2020-09-30 13:03:57 -0500

    added a fade out effect

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ContextualMessageController : MonoBehaviour
 {
     [SerializeField]
-    private float fadeTime;
+    private float fadeDuration = 1;
 
     private TMP_Text messageText;
     private CanvasGroup canvasGroup;
@@ -24,7 +24,18 @@ public class ContextualMessageController : MonoBehaviour
     {
         messageText.text = message;
         canvasGroup.alpha = 1;
+        float fadeStartTime;
+        float elapsedTime = 0;
         yield return new WaitForSeconds(duration);
+        fadeStartTime = Time.time;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime = Time.time - fadeStartTime;
+            canvasGroup.alpha = 1 - elapsedTime / fadeDuration;
+            yield return null;
+        }
+
         canvasGroup.alpha = 0;
     }
 }
