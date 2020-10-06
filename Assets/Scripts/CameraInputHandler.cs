8adf3046c252d4296a5b16eac5358dc3e10f9e8f
David Antognoli <david.antognoli@gmail.com>
2020-10-06 14:48:15 -0500

    fixed mouse look with new input system
    - must scale vector!
    - there's a built in thing for this in Unity 2020 https://docs.unity3d.com/Packages/com.unity.cinemachine@2.6/manual/CinemachineAlternativeInput.html#:~:text=Some%20Cinemachine%20components%20%28e.g.%20FreeLook%2C%20POV%2C%20OrbitalTransposer%29%20require,input%20by%20querying%20the%20standard%20Unity%20Input.GetAxis%20%28name%29.

--- Assets/Scripts/CameraInputHandler.cs
+++ Assets/Scripts/CameraInputHandler.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using Cinemachine;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraInputHandler : MonoBehaviour
 {
+    private PlayerInput playerInput;
+    private CinemachineFreeLook cinemachineFreeLook;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cinemachineFreeLook = GetComponent<CinemachineFreeLook>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnLook(InputAction.CallbackContext context)
     {
-        
+        var input = context.ReadValue<Vector2>();
+        Debug.Log($"Look X: {input.x} Mouse Y: {input.y}");
+        cinemachineFreeLook.m_XAxis.Value += input.x;
+        cinemachineFreeLook.m_YAxis.Value += input.y;
     }
 }
